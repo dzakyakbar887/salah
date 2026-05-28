20bd3981ea0dd197c38f61b4e7d493a84ea0a232
dzakyakbar887 <dzakyakbar392@gmail.com>
2026-05-28 10:38:02 +0700

    tambah awal

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -13,3 +13,39 @@ struct Node {
     Menu data;
     Node* next;
 };
+
+Node* head = NULL;
+
+void tambah_awal() {
+    Node* baru;
+    baru = new Node;
+    
+    cout << "Nama Makanan: ";
+    cin >> baru->data.nama_makanan;
+    cout << "Harga: ";
+    cin >> baru->data.harga_makanan;
+    
+    // Validasi input kategori
+    if (baru->data.nama_makanan == "" || baru->data.harga_makanan <= 0) {
+        cout << "Input tidak valid" << endl;
+        return;
+    }
+    
+    // Pilih Kategori Barang
+    int pilih_kategori;
+    cout << "Pilih Kategori:" << endl;
+    cout << "1. Makanan" << endl;
+    cout << "2. Minuman" << endl;
+    cout << "Pilihan: ";
+    cin >> pilih_kategori;
+    
+    // Pengkondisian untuk menentukan isi dari kategori berdasarkan pilihan user
+    if (pilih_kategori == 1) {
+        baru->data.kategori = "Makanan";
+    } else {
+        baru->data.kategori = "Minuman";
+    }
+    
+    baru->next = head;
+    head = baru;
+}
